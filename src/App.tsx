38cddd50eec7fa087ec59e83c959aeacbf8a8686
Andrew Huntington <andrew.huntington@gmail.com>
2022-02-02 16:36:25 +0900

    Finish TitleBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TitleBar from './components/TitleBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-[550px] h-[704px] border-2 border-solid border-red-600 mx-auto'>
+      <TitleBar />
+      <h1 className='text-3xl font-bold underline'> Hello world! </h1>
     </div>
   );
 }
---
+++ src/components/TitleBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../images/logo.png';
+
+export default function TitleBar() {
+  return (
+    <div className='h-14 mb-9 bg-[#F55A5A] flex justify-center items-center'>
+      <img className='h-6 mr-2' src={logo} alt='logo' />
+      <h1 className='text-white text-sm font-normal'>my travel journal.</h1>
+    </div>
+  );
+}
